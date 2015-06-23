f283c09ee29fbb1eda1c8f5a816f0a80597c5378
andou <antonio.pastorino@gmail.com>
2015-06-23 15:26:06 +0200

    PhpUnit configurations

--- unittests/SuperImageResizerTest.php
+++ unittests/SuperImageResizerTest.php
@@ -1,10 +1,11 @@
+<?php
 
 class SuperImageResizerTest extends PHPUnit_Framework_TestCase
 {
 	/**
 	 * @author antonio.pastorino
 	 */
-	public function testWorks()
+	public function test_works()
 	{
 		$this->assertTrue(true);
 	}
