3146508053e73652cea83166353849f7de2a9574
andou <antonio.pastorino@gmail.com>
2015-07-14 17:50:10 +0200

    index sample

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
+
+/*
+Sample:
+*/
+/*
 require_once './vendor/autoload.php';
 $basedir = __DIR__;
 new \Andou\Autoloader\Autoloader($basedir . "/src");
 $app = \Andou\SuperImageResizer\App::getInstance($basedir, '/config/config.ini');
 $_newimg = $app->getImageResized('test.jpg', "1200");
 $app->serve($_newimg);
+*/
